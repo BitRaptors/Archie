a2668adf4a713112617f4dd8765735d529daa3cd
Gabor Bakos <gabor@bitraptors.com>
2026-04-13 21:15:17 +0200

    2 level

--- share/viewer/src/main.tsx
+++ share/viewer/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import HomePage from './pages/HomePage'
+import CoverPage from './pages/CoverPage'
 import ReportPage from './pages/ReportPage'
 import NotFoundPage from './pages/NotFoundPage'
 
@@ -11,7 +12,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/r/:token" element={<ReportPage />} />
+        <Route path="/r/:token" element={<CoverPage />} />
+        <Route path="/r/:token/details" element={<ReportPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
